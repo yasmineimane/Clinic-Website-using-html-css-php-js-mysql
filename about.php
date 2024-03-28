aafff16baca274ed71d4b442f9d17c1f8830d204
aicha <aichamek318@gmail.com>
2024-03-28 02:22:50 +0100

    up

--- about.php
+++ about.php
@@ -7,13 +7,11 @@
     <link rel="stylesheet" href="./style/about.css">
 </head>
 <body>
-    <header>
-        <div class="head">
-            <img src="./images/logo.png" alt="logo"width="70px"height="70px">
-            <h3>Health Care</h3>
-        </div>
+    <?php include("nav.php"); ?>
+    
+    <div class="head">
         <h1>Celebrating Excellence in Healthcare Unveiling Our Story</h1>
-    </header>
+    </div>
     <main>
         <section class="contentOne">
             <h1>Innovative Compassion: Our Commitment to Exceptional Healthcare</h1>
@@ -39,60 +37,60 @@
         <section class="contentThree">
             <img src="./images/history.jpg" alt="history">
             <div class="Three">
-            <h1>Our History</h1>
-            <p>Established in [Year], [Hospital Name] has been a cornerstone of healthcare in [City/Region]. 
+                <h1>Our History</h1>
+                <p>Established in [Year], [Hospital Name] has been a cornerstone of healthcare in [City/Region]. 
                 From our humble beginnings to our current state-of-the-art facilities,
                 we have remained dedicated to serving our community with the highest standards of care.
-            </p>
+                </p>
             </div>
         </section>
         <section class="contentFour">
             <div class="Four">
-            <h1>Leadership Team</h1>
-            <p>Meet the dedicated professionals who lead [Hospital Name] in its mission to provide exceptional healthcare services:
+                <h1>Leadership Team</h1>
+                <p>Meet the dedicated professionals who lead [Hospital Name] in its mission to provide exceptional healthcare services:
                 [CEO Name], Chief Executive Officer
                 [Medical Director Name], Medical Director
                 [Administrator Name], Hospital Administrator
-            </p>
-                </div>
-                <img src="./images/Medical-doctors.jpg" alt="Medical-doctors">
+                </p>
+            </div>
+            <img src="./images/Medical-doctors.jpg" alt="Medical-doctors">
         </section>
         <section class="contentFive">
             <img src="./images/Facilities.jpg" alt="Facilities">
             <div class="Five">
-            <h1>Facilities</h1>
-            <p>Our hospital is equipped with state-of-the-art technology and modern facilities to ensure the comfort and well-being of our patients.
+                <h1>Facilities</h1>
+                <p>Our hospital is equipped with state-of-the-art technology and modern facilities to ensure the comfort and well-being of our patients.
                 From our advanced surgical suites to our cozy patient rooms, 
                 every aspect of our hospital is designed with your care in mind.
-            </p>
+                </p>
             </div>
         </section>
         <section class="contentSix">
             <div class="Six">
-            <h1>Why Choose [Hospital Name]?</h1>
-            <p>Compassionate Care. <br>
+                <h1>Why Choose [Hospital Name]?</h1>
+                <p>Compassionate Care. <br>
                 Innovative Approach. <br>
                 Patient-Centered Philosophy. <br>
                 Comprehensive Services. <br>
                 Community Connection. <br>
                 Commitment to Excellence.
-            </p>
-                </div>
-                <img src="./images/whyus.avif" alt="why us">
+                </p>
+            </div>
+            <img src="./images/whyus.avif" alt="why us">
         </section>
     </main>
     <footer>
         <div class="end">
-        <h2>15+</h2>
-        <p>Years of Experience</p>
+            <h2>15+</h2>
+            <p>Years of Experience</p>
         </div>
         <div class="end">
-        <h2>1000+</h2>
-        <p>Successful Surgeries</p>
+            <h2>1000+</h2>
+            <p>Successful Surgeries</p>
         </div>
         <div class="end">
-        <h2>24/7</h2>
-        <p>Emergency Services</p>
+            <h2>24/7</h2>
+            <p>Emergency Services</p>
         </div>
     </footer>
 </body>
