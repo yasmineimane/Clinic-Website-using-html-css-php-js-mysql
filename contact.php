2ed042dc6e698d732ad86c0bcfbf5bbf2cf8959f
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-04-27 17:16:57 +0200

    new

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="./style/contact.css">
+    <link rel="stylesheet" href="./style/Contact.css">
     <title>Contact</title>
 </head>
 <body>
@@ -16,47 +16,7 @@
             <p>Send us a message</p>
         </div>
         <div class="container">
-            <!-- <section class="contact-form">
-                <p>Fill out the form below and we'll be in touch soon. </p>
-                <form action="#" method="POST">
-                    <label for="First Name">First Name*</label>
-                    <input type="text" id="first Name" name="firstName" required>
-                    <label for="Last Name">Last Name*</label>
-                    <input type="text" id="last Name" name="lastName" required>
-                    <label for="Phone">Phone*</label>
-                    <input type="phone" id="phone" name="phone" required>
-                    <label for="email">Email*</label>
-                    <input type="email" id="email" name="email" required>
-                    <label for="message">Message*</label>
-                    <textarea id="message" name="message" rows="5" required></textarea>
-                    <button type="submit">Send Message</button>
-                </form>
-            </section> -->
             <section class="contact-info">
-                <div class="c-info">
-                    <div class="info">
-                        <h3><i class="fas fa-phone"></i>Call US</h3>
-                        <p>+123 456 789</p>
-                    </div>
-                    <div class="info">
-                        <h3><i class="fas fa-envelope"></i> Email Us</h3>
-                        <a href="mailto:our email">Healthcare02@Gmail.com</a>
-                    </div>
-                    <div class="info">
-                        <h3><i class="fas fa-map-marker-alt"></i>Our Location</h3>
-                        <p>123 Main Street,Zeboudj,Chlef,Algeria</p>
-                    </div>
-                </div>
-                <div class="Smedia">
-                    <div class="info">
-                        <h3>Our Social Media</h3>
-                    </div>
-                    <div class="icons">
-                        <a href="https://www.facebook.com/your_facebook_profile" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                        <a href="https://www.instagram.com/your_instagram_profile" target="_blank"><i class="fab fa-instagram"></i></a>
-                        <a href="https://www.linkedin.com/your_linkedin_profile" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                        <a href="https://wa.me/your_whatsapp_number" target="_blank"><i class="fab fa-whatsapp"></i></a>    
-                    </div>
                 <div class="doccontact">
                     <h3>Our Doctors Contact</h3>
                     <div class="contact">
@@ -86,6 +46,32 @@
                         </div>
                     </div>
                 </div>
+                <div class="c-info">
+                    <h3>Our Contact</h3>
+                    <div class="c-infocon">
+                        <div class="info">
+                            <h3><i class="fas fa-phone"></i>Call US</h3>
+                            <p>+123 456 789</p>
+                        </div>
+                        <div class="info">
+                            <h3><i class="fas fa-envelope"></i> Email Us</h3>
+                            <a href="mailto:our email">Healthcare02@Gmail.com</a>
+                        </div>
+                        <div class="info">
+                            <h3><i class="fas fa-map-marker-alt"></i>Our Location</h3>
+                            <p>123 Main Street,Zeboudj,Chlef,Algeria</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="Smedia">
+                    <h3>Our Social Media</h3>
+                    <div class="icons">
+                        <a href="https://www.facebook.com/your_facebook_profile" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://www.instagram.com/your_instagram_profile" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.linkedin.com/your_linkedin_profile" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://wa.me/your_whatsapp_number" target="_blank"><i class="fab fa-whatsapp"></i></a>    
+                    </div>
+                
             </section>
         </div>
     </main>
